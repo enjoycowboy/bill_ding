9f41b947b4d2c9b533740c7203552129e1a11bec
Eduardo Veiga <esveiga@blockbit.com>
2024-07-29 21:07:58 -0300

    base: walls can now be clicked...
    and even hover. Now, to find out how to
    create a base cell from scratch

--- scripts/wall_click_detect.gd
+++ scripts/wall_click_detect.gd
@@ -1,28 +1,20 @@
-extends MeshInstance3D
-var space_state;
-var camera : Camera3D;
-var mouse_pos;
-var ray_result;
-var butpress : bool = false;
-var params;
-func _generate_ray():
-	butpress = true
-	mouse_pos = get_viewport().get_mouse_position();
-	var from = camera.project_ray_origin(mouse_pos);
-	var to = camera.project_ray_normal(mouse_pos) * camera.far;
-	space_state = get_world_3d().direct_space_state;
-	params = PhysicsRayQueryParameters3D.create(from, to);
-	ray_result = space_state.intersect_ray(params)
-	return ray_result
+extends Node3D
+var selectable = true;
+@onready var mesh = get_node(".");
 
-func _physics_process(delta):
-	if Input.is_action_just_pressed("ui_wheel_click"):
-		var ray_result = _generate_ray()
-		if ray_result:
-			_digest_collision(params)
+func _on_static_body_3d_input_event(camera, event, position, normal, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("Click Wall")
+			print(mesh)
+			
+	pass # Replace with function body.
 
 func _ready():
-	camera = get_node("InnerAxis/myCamera");
+	pass
+	
+func _on_static_body_3d_mouse_entered():
+	pass # Replace with function body.
 
-func _digest_collision(params):
-	print(params)
+func _on_static_body_3d_mouse_exited():
+	pass # Replace with function body.
